e1c9eddbbc9cd5032f68acb405a2080bc814273a
TanjaStankovic <tanja.pisces@gmail.com>
2016-12-13 12:01:52 +0100

    test

---
+++ Tanja07-Korisnicki unos/src/App.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Kreiranje skener objekta
+		Scanner input = new Scanner(System.in);
+		
+		// Output prompt
+		System.out.println("Unesite ime: ");
+		String line = input.nextLine();
+	 System.out.println("Unesite prezime: ");
+	 String line1 = input.nextLine();
+	 System.out.println("Unesite godinu rodjenja: ");
+		// Izvrsavanje skenera i arhiviranje rezultata
+		String line2 = input.nextLine();
+		// int value = input.nextInt();
+		// int value = input.nextDouble();
+		
+		// Ispis arhiviranog rezultata
+		System.out.println("Zdravo: " + line +" " + line1 + " " +"koja si rodjena" +" " + line2);
+		
+		// Svaki skener i citac treba zatvoriti nakon upotrebe (dobra praksa)
+		input.close();
+			
+		/*
+		 * Zadatak:
+		 * 1 - Napisati program koji omogucava korisniku da unese svoje Ime, Prezime, Godinu rodjenja i pol.
+		 *   - Nakon sto korisnik unese trazene podatke ispisati jednu recenicu kojom se ponavlja korisnicki unos.
+		 */
+	}
+
+}
